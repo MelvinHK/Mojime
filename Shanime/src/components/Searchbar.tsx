3b8c4def9a4e08a6076fccd015530ae579d71de4
Melvin <melvinhkusuma@gmail.com>
2024-04-21 22:22:21 +1000

    search results list

--- Shanime/src/components/Searchbar.tsx
+++ Shanime/src/components/Searchbar.tsx
@@ -1,25 +1,43 @@
 import { getAnimeSearch } from "../utils/api";
 import { useState } from "react";
+import { IAnimeResult } from "@consumet/extensions";
 
 function Searchbar() {
   const [query, setQuery] = useState("");
+  const [resultsList, setResultsList] = useState<IAnimeResult[]>();
 
   const handleSubmit = async () => {
-    const results = await getAnimeSearch(query);
-    console.log(results);
+    const results = await getAnimeSearch(query, 1);
+
+    console.log(results?.results);
+    setResultsList(results?.results);
   }
 
   return (
-    <form spellCheck='false' onSubmit={(e) => {
-      e.preventDefault();
-      handleSubmit();
-    }}>
-      <input placeholder='Search'
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        autoFocus
-      />
-    </form>
+    <>
+      <form spellCheck='false' onSubmit={(e) => {
+        e.preventDefault();
+        handleSubmit();
+      }}>
+        <input
+          className={`${resultsList ? `flat-bottom-br` : ``}`}
+          placeholder='Search'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          autoFocus
+        />
+        {resultsList && <div className="relative w-100">
+          <ul id='search-results'>
+            {resultsList.map(result =>
+              <li key={result.id}>
+                {result.title as string}
+              </li>
+            )}
+          </ul>
+        </div>}
+      </form>
+
+    </>
   )
 }
 
--- Shanime/src/utils/api.tsx
+++ Shanime/src/utils/api.tsx
@@ -2,11 +2,11 @@ import { ANIME } from "@consumet/extensions";
 
 const provider = new ANIME.Gogoanime();
 
-export const getAnimeSearch = async (query: string) => {
+export const getAnimeSearch = async (query: string, page: number) => {
   try {
-    const results = await provider.search(query);
-    
+    const results = await provider.search(query, page);
     return results;
+
   } catch (error) {
     console.log(error);
   }
