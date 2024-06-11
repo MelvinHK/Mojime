35ef192afb50859cebdf9349e8a800a98435f16b
Melvin <melvinhkusuma@gmail.com>
2024-06-11 17:05:42 +1000

    removed unused values

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -13,26 +13,22 @@ type WatchContextType = {
   animeInfo: IAnimeInfo | undefined,
   setAnimeInfo: Dispatch<SetStateAction<IAnimeInfo | undefined>>,
   isFullscreen: MutableRefObject<boolean>,
-  isAutoFullscreen: MutableRefObject<boolean>,
 }
 
 export const WatchContext = createContext<WatchContextType>({
   animeInfo: undefined,
   setAnimeInfo: () => { },
-  isFullscreen: { current: false },
-  isAutoFullscreen: { current: false }
+  isFullscreen: { current: false }
 });
 
 function Root(props: RootProps) {
   const [animeInfo, setAnimeInfo] = useState<IAnimeInfo>();
   const isFullscreen = useRef(false);
-  const isAutoFullscreen = useRef(false);
 
   const WatchContextValues: WatchContextType = {
     animeInfo,
     setAnimeInfo,
     isFullscreen,
-    isAutoFullscreen
   }
 
   const location = useLocation();
