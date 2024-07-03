0fa0b4606ab74640667935350a0ad22486af75c8
Melvin <melvinhkusuma@gmail.com>
2024-07-03 15:07:31 +1000

    moved random kaomoji index inside function

--- client/src/pages/WatchLayout.tsx
+++ client/src/pages/WatchLayout.tsx
@@ -2,14 +2,12 @@ import Watch from "../components/Watch"
 import { kaomojis } from "./Home"
 import { Link } from "react-router-dom"
 
-const randomIndex = Math.floor(Math.random() * kaomojis.length);
-
 export default function WatchLayout() {
   return (
     <>
       <Watch />
       <Link to="/" id="home">
-        {kaomojis[randomIndex]}
+        {kaomojis[Math.floor(Math.random() * kaomojis.length)]}
       </Link>
     </>
   )
