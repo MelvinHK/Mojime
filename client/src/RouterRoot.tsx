125e50947bf627c0e1c2ced0c626181c6354c423
Melvin <melvinhkusuma@gmail.com>
2024-09-12 14:47:49 +1000

    re-added strictmode

--- client/src/RouterRoot.tsx
+++ client/src/RouterRoot.tsx
@@ -44,7 +44,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <RouterProvider router={router} />
-  // </React.StrictMode>
+  </React.StrictMode>
 );
