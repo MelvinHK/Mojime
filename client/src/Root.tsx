e99a768d60155247b6d717ffebdbbd7296318a2f
Melvin <melvinhkusuma@gmail.com>
2025-01-26 21:07:51 +1100

    added broken sources announcement

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -3,12 +3,18 @@ import { ErrorBoundary } from "react-error-boundary";
 import Header from "./components/Header";
 import Error from "./pages/Error";
 import { WatchProvider } from "./contexts/WatchProvider";
+import { useEffect } from "react";
 
 interface RootProps {
   routeError?: JSX.Element;
 }
 
 function Root(props: RootProps) {
+
+  useEffect(() => {
+    alert("Announcement:\nThe video sources that this site extracts from have become broken/unavailable, so you will be unable to view any anime at this time. A refactor to extract from a different source is planned. Thank you for using Mojime!\n<(＿　＿)>~gomennasai");
+  }, []);
+
   const location = useLocation();
 
   return (
